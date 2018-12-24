b7b887833e4337c528a5432dc81cdbaf9388e1fc
dwarak1994 <badshahreddy@gmail.com>
2018-12-23 21:47:54 -0500

    basics

---
+++ Basics.py
@@ -0,0 +1,103 @@
+
+"""
+print("hello world")
+print(2/2)
+print("hello world "+ str(2))
+
+print(str(22)+str(2))
+
+"""
+#print("hello")
+# this is single line comment
+
+"""
+multi line comment
+"""
+
+## data types
+##100, 22, 22.5-->int ,+,-,/,* int, float
+##"dwarak", "prasad"--> strings
+
+## loops
+
+
+
+
+"""
+num=3
+
+## check if numner is odd or even
+
+if num%2==0:
+    print("even")
+else:
+    print("odd")
+
+if num-2==6:
+    print ("yes")
+else:
+    print ("no")    
+"""
+#=, ==
+#
+"""
+restro='I'
+dish="k"
+if restro=='I':
+    print("i")
+    if dish=="k":
+        print("k")
+    else:
+        print("not k")
+elif restro=='C':
+    print("d")
+elif restro=='S':
+    print("s")
+else:
+    print("A")
+"""
+
+
+## for loop
+
+"""
+i=6
+for i in range(5):
+    print(i*2)
+"""
+
+#[]
+"""
+x=["apple","pine","mango"]
+#print(x[2])
+x.append("orange")
+
+#print(x)
+x.append("custard")
+#print(x)
+
+for i in range(len(x)):
+    print(x[i], i)
+"""
+
+#set=()
+#x=[0,1,1,1,2,3,3,4,4,4,99,99]
+#print(set(x))
+
+
+
+
+# apple: its a fruit
+#map
+
+"""
+new_dict={}
+new_dict['venkat']="prasad"
+print(new_dict)
+new_dict["dwarak"]="nath"
+print(new_dict)
+"""
+
+##
+
+## import
--- RestEx.py
+++ RestEx.py
@@ -12,3 +12,7 @@ def hello():
 
 if __name__=='__main__':
     app.run(debug=True)
+
+
+## REST API
+
